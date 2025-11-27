8405cad459f80176c6bdf77e13b9658df5c09949
Otavius Whitehead <developer.otavius@gmail.com>
2025-11-26 21:03:35 -0500

    chore: updated views and the urls

--- account_user/urls.py
+++ account_user/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from account_user.views import UserListView
 
 urlpatterns = [
-    
+    path("", UserListView.as_view(), name="base")
 ]
--- account_user/views.py
+++ account_user/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from account_user.models import UserProfile
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 
 # Create your views here.
+class UserListView(LoginRequiredMixin, ListView):
+    template_name = "account_user/base.hmtl"
+    model = UserProfile
+    context_object_name = "user"
