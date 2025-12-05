6121e5ff321f32e2b0e17dabe071f8bf88aaf233
Otavius Whitehead <developer.otavius@gmail.com>
2025-12-05 05:46:10 -0500

    chore: added new model; no migrations yet

--- app/models.py
+++ app/models.py
@@ -6,3 +6,11 @@ from django.contrib.auth.models import AbstractUser
 class UserProfile(AbstractUser):
     pass 
 
+class Asset(models.Model):
+    symbol = models.CharField(max_length=10, unique=True)
+    company_name = models.CharField(max_length=128, null=True, blank=True)
+    asset_class = models.CharField(max_length=20)
+    exchange = models.CharField(max_length=20)
+    status = models.CharField(max_length=20) #active/inactive
+    tradeable=models.BooleanField(default=True)
+     
